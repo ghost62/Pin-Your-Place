ede86382ccaadae558b555cc9a82d098b0d3f765
Your Name <you@example.com>
2020-11-11 01:17:21 +0530

    final commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -27,7 +27,7 @@ const Header = ({ classes }) => {
               color="inherit"
               noWrap
             >
-              GeoPins
+             Pin Your Place
             </Typography>
           </div>
 
